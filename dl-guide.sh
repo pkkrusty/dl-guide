bba4bb8a660c195a2ec771c4ac3bf794c14206a5
Zach Butler <kj4ezj@pm.me>
2024-02-12 17:00:22 -0500

    Write simple log function to prepend date and script name to log lines

--- dl-guide.sh
+++ dl-guide.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -eo pipefail
-echo "$(date '+%F %T %Z') ${0##*/} - Begin."
+
+# prepend timestamp and script name to log lines
+function log {
+    echo "$(date '+%F %T %Z') ${0##*/} - $*"
+}
+
+log 'Begin.'
 source ./deps/bin/ee
 
 export ZAP2XML_CMD="/zap2xml.pl -u '$ZAP2IT_USERNAME' -p '$ZAP2IT_PASSWORD' -U -o /data/tv-guide.xml"
 
 ee "docker run -v '$JELLYFIN_METADATA_DIR/guide:/data' shuaiscott/zap2xml /bin/sh -c \"$ZAP2XML_CMD\""
 
-echo "$(date '+%F %T %Z') ${0##*/} - Done."
+log 'Done.'
 # Copyright © 2024 Zach Butler - https://github.com/kj4ezj/jellyfin-tv-guide
