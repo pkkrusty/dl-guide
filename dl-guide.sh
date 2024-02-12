adc0ff1fdd5c8bb97362136623d5797bb44ae5ec
Zach Butler <kj4ezj@pm.me>
2024-02-12 17:30:02 -0500

    Fail if echo-eval is not available

--- dl-guide.sh
+++ dl-guide.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 set -eo pipefail
 
+# check for dependencies
+function check-deps {
+    if [[ -f ./deps/bin/ee ]]; then
+        log 'Found "./deps/bin/ee".'
+        source ./deps/bin/ee
+    elif ee >/dev/null 2>&1; then
+        log 'Found "ee" (echo-eval) in the environment.'
+    else
+        log '\e[1;31mFATAL: Missing dependency "ee" (echo-eval)!\e[0m'
+        log 'See documentation: https://github.com/kj4ezj/jellyfin-tv-guide'
+        exit 127
+    fi
+}
+
 # prepend timestamp and script name to log lines
 function log {
     printf "\e[0;30m%s ${0##*/} -\e[0m $*\n" "$(date '+%F %T %Z')"
 }
 
 log 'Begin.'
-source ./deps/bin/ee
-
+check-deps
 export ZAP2XML_CMD="/zap2xml.pl -u '$ZAP2IT_USERNAME' -p '$ZAP2IT_PASSWORD' -U -o /data/tv-guide.xml"
 
 ee "docker run -v '$JELLYFIN_METADATA_DIR/guide:/data' shuaiscott/zap2xml /bin/sh -c \"$ZAP2XML_CMD\""
